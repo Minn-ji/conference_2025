7ee53660967212fcb26564815a6323e675e71a7e
yunjaeekim <kyj9396922@gmail.com>
2025-11-05 17:24:26 +0900

    update for evaluate-api_v2

--- SOUP/main.py
+++ SOUP/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from api.v1.routers import api_router
 import time
-import uvicorn
 
 app = FastAPI(
     title="SOUP API",
@@ -16,6 +15,3 @@ app.include_router(api_router, prefix="/v1")
 def read_root():
     time.sleep(30)
     return {"comment": "Hello, It's SOUP API!", "status": "ok"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- SOUP/schema/__init__.py
+++ SOUP/schema/__init__.py
@@ -1,12 +1,12 @@
-#from schema.payload_schema import *
-#from schema.create_payload import *
+from schema.payload_schema import *
+from schema.create_payload import *
 from schema.ocr_schema import *
 from schema.grade_schema import *
 __any__ = [
-    #'GeneratePlannerRequest',
-    #'GenerateQuizRequest',
-    #'EvaluateQuizRequest',
-    #'create_planner_input_payload',
+    'GeneratePlannerRequest',
+    'GenerateQuizRequest',
+    'EvaluateQuizRequest',
+    'create_planner_input_payload',
     'OCRRequest',
     'OCRResponse',
     'GradeRequest',
